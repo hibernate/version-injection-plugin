10b85838ac2a64f2e704dd7f930e05714abc270b
marko-bekhta <marko.prykladna@gmail.com>
2026-01-29 23:57:04 +0100

    Do not use getProject during execution

--- src/main/java/org/hibernate/build/gradle/inject/InjectionSpec.java
+++ src/main/java/org/hibernate/build/gradle/inject/InjectionSpec.java
@@ -1,31 +1,31 @@
 package org.hibernate.build.gradle.inject;
 
-import javax.inject.Inject;
-
 import org.gradle.api.Project;
-import org.gradle.api.model.ObjectFactory;
 import org.gradle.api.provider.ListProperty;
+import org.gradle.api.provider.Property;
 
 /**
  * @author Steve Ebersole
  */
 public class InjectionSpec {
 	private final ListProperty<TargetMember> targetMembers;
-
-	@Inject
-	public InjectionSpec(ObjectFactory objects) {
-		targetMembers = objects.listProperty( TargetMember.class );
-	}
+    private final Property<String> projectVersion;
 
 	public InjectionSpec(Project project) {
-		this( project.getObjects() );
+        this.targetMembers = project.getObjects().listProperty( TargetMember.class );
+        this.projectVersion =  project.getObjects().property( String.class )
+                .convention(project.getVersion().toString());
 	}
 
 	public ListProperty<TargetMember> getTargetMembers() {
 		return targetMembers;
 	}
 
-	public void into(String className, String member) {
+    public Property<String> getProjectVersion() {
+        return projectVersion;
+    }
+
+    public void into(String className, String member) {
 		into( new TargetMember( className, member ) );
 	}
 
